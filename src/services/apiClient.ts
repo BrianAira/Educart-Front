7ffa489f7a2ea103b90a4cdbd97f3180e9a37c31
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-01-22 20:43:17 -0300

    fix: se corrigio error de instancia de token en localstorage

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -13,7 +13,12 @@ export const apiClient = axios.create({
 
 apiClient.interceptors.request.use(
   (config) => {
-    const token = store.getState().auth.token;
+    let token = store.getState().auth.token;
+    if (!token) {
+      token = localStorage.getItem("token");
+    }
+
+    console.log(`🚀 Petición a: ${config.url} | Token: ${token ? 'PRESENTE' : 'AUSENTE'}`);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -25,12 +30,24 @@ apiClient.interceptors.request.use(
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    
+    const isAuthError=error.response?.status==401;
+    const url = error.config?.url||"";
+    const isAdminRoute=url.includes("admin");
+    // const isAdminRoute=error.config.VITE_API_URL.includes('admin')
+    if (isAuthError && !isAdminRoute) {
       store.dispatch(logout());
       localStorage.removeItem("token");
       localStorage.removeItem("user");
-      window.location.href = "/login";
+      // window.location.href = "/login"; // Evita usar esto, mejor usa navigate de React Router
     }
     return Promise.reject(error);
+    // if (error.response?.status === 401) {
+    //   store.dispatch(logout());
+    //   localStorage.removeItem("token");
+    //   localStorage.removeItem("user");
+    //   window.location.href = "/login";
+    // }
+    // return Promise.reject(error);
   }
 );
