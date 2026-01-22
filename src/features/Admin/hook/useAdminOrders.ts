82c2388b39ac68d4023fed7ce4ef21c9af24ed0b
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-01-22 20:35:49 -0300

    fix: se corrigio ruta del endpoint order

--- src/features/Admin/hook/useAdminOrders.ts
+++ src/features/Admin/hook/useAdminOrders.ts
@@ -1,14 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { apiClient } from "../../../services/apiClient";
 import { toOrder } from "../../Order/api/mappers";
-// import { IOrder } from "../../features/Order/types/Order"; // Usa tus tipos existentes
-// import { toOrder } from "../features/Order/api/mappers"; // Usa tus mappers
 
 export const useAdminOrders = () => {
     return useQuery({
         queryKey: ["admin-orders"],
         queryFn: async () => {
-            const { data } = await apiClient.get("/orders/admin/all");
+            const { data } = await apiClient.get("/orders/admin/all/");
             return data.map(toOrder); // Mapeamos snake_case a camelCase
         },
     });
