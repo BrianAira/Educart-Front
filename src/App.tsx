55ea78a2520726ecf5968ddcbfb71c3ed697163b
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-01-22 20:45:37 -0300

    fix: se añadio componentes de HeroUI y ToastProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import './App.css'
+// import './App.css'
 import {Provider} from "react-redux";
-import { store } from './store';
+// import { store } from './store';
 import { queryClient } from './lib/queryClient';
 import { AppRouter } from './routes/AppRouter';
+import { ToastProvider } from './components/ui/ToastProvider';
+import { useTheme } from './features/Settings/hooks/useTheme';
+import './index.css'
+
+import { HeroUIProvider } from '@heroui/react';
+import { store } from './store/store';
 export const App = () => {
-  
+  const [theme]=useTheme() 
   return (    
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-
-        <AppRouter />
+        <HeroUIProvider>
+          <ToastProvider>
+            <div className={`${theme} min-h-screen bg-background text-foreground`}>
+              <AppRouter />
+            </div>
+            
+          </ToastProvider>
+        </HeroUIProvider>
+        
+        
       </QueryClientProvider>
     </Provider>
 
